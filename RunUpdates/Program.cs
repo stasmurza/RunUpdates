f32b044dc41d002530dddf11f4807c12a090df9c
stasmurza <stasmurza@gmail.com>
2017-04-28 09:14:08 +0300

    Add project files.

---
+++ RunUpdates/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace InstallUpdates
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                GetAndInstallUpdates getAndInstallUpdates = new GetAndInstallUpdates(args);
+                getAndInstallUpdates.Start();               
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine(exception.ToString());
+                Console.ReadLine();
+                throw;
+            }
+        }
+
+    }
+}
